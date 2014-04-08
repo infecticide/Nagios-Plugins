56b4986ffc11339f72894d8975b3cb3f24c971ed
infecticide <tuxsteve@gmail.com>
2014-04-08 12:46:46 -0600

    Updated script to include more error handling and allows checking remote systems

--- check_securitylog.ps1
+++ check_securitylog.ps1
@@ -1,7 +1,40 @@
-# Is log accessible?
-$log_entry = Get-WinEvent -LogName Security -MaxEvents 1 -ErrorAction SilentlyContinue
+param($computer,$logname,$timeperiod)
+
+$scriptname = $MyInvocation.MyCommand.Name
+
+# Check parameters, make sure all are filled out
+if($computer -eq $null -or $logname -eq $null -or $timeperiod -eq $null) {
+    write-host "Parameters missing"
+    write-host " "
+    write-host "Example:"
+    write-host " "
+    write-host "$scriptname <hostname> <logname> <timeperiod>"
+    write-host " "
+    write-host "<hostname> - the DNS name of the machine you want to check"
+    write-host "<logname> - the Windows EventLog you want to check"
+    write-host "<timeperiod> - how far back to check for events in minutes"
+    write-host " "
+    exit 1
+    }
+
+# Is Server reachable?
+ping -n 1 $computer | out-null
 if($? -ne $TRUE) {
-    Write-Host "Security Log is NOT accessible"
+    write-host "ERROR - Host is unreachable"
+    exit 1
+    }
+
+# Does log exist?
+$available_logs = (Get-WinEvent -ListLog * -ComputerName $computer).LogName
+if($available_logs -notcontains $logname) {
+    write-host "ERROR - $logname Log does not exist on this host"
+    exit 1
+    }
+
+# Is log accessible?
+$log_entry = Get-WinEvent -LogName $logname -MaxEvents 1 -ComputerName $computer -ErrorAction SilentlyContinue
+if($? -ne $TRUE) {
+    Write-Host "ERROR - $logname Log is not accessible"
     exit 1
     }
 
@@ -9,11 +42,11 @@ if($? -ne $TRUE) {
 $timeCreated = $log_entry.TimeCreated
 $now = get-date
 $difference = New-TimeSpan -Start $timeCreated -End $now
-$diff_mins = $difference.Minutes
-if($diff_mins -gt 5) {
-    Write-Host "Security Log has no new data in the last 5 minutes"
+$diff_mins = $difference.TotalMinutes
+if($diff_mins -gt $timeperiod) {
+    Write-Host "ERROR - $logname Log has no new data in the last $timeperiod minutes"
     exit 1
     } ELSE {
-    Write-Host "Security Log is accessible and current"
+    Write-Host "OK - $logname Log is accessible and current"
     exit 0
     }
